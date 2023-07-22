1a2c0b0b0b47bf88b177d9d18a183cd469462c90
kiranthedevelopr <kirannasim05@gmail.com>
2023-07-22 04:12:29 -0700

    14-16

---
+++ app.js
@@ -0,0 +1,168 @@
+//Question # 01
+//let arr = [];
+
+
+
+//Question # 02
+//let arr_1 = [];
+
+
+//Question # 03
+//let str_arr = ["kiran","ayat","abeeha"];
+
+//Question # 04
+//let num_arr = [1,2,3];
+
+//Question # 05
+//let bool_arr = [true,false];
+
+//Question # 06
+//let mixed_arr =["kiran","ayat","abeeha",1,2,3];
+
+//Question # 07
+//let qual_arr = ["SSC","HSC","BCS","BS","B.COM","MS","M.Phil","Phd"];
+//document.write("<h2> qualification </h2>");
+
+//for(i=0; i<qual_arr.length; i++){
+    //document.write("(",i+1,"))");
+    //document.write(qual_arr[i],"<br>");
+//}
+
+//Question # 08
+//let name_arr = ["kiran","ayat","abeeha"];
+
+//let scored_marks =  [490,492,493];
+
+//document.write("scored marks of",name_arr[0],"are",scored_marks[0],". percentage:",(scored_marks[0]*100)/500,"<br>");
+//document.write("scored marks of",name_arr[1],"are",scored_marks[1],"are percentage:"),(scored_marks[1],". percentage:",(scored_marks[1]*100)/500,"<br>");
+//document.write("scored marks of",name_arr[2],"are",scored_marks[2],". percentage:",(scored_marks[2]*100)/500,"<br>");
+
+//Question # 09 re check
+// let arr = ["blue","green","red","white","brwon","pink","black","maroon"];
+// console.table("old array",arr);
+// part => a
+// let begining_color = prompt ("enter the color name you want to add at begining");
+
+// arr.unshift(begining_color);
+// console.table("added the color at the begining of array",arr);
+
+// part =>b 
+// let end_color = propmt("enter the color name you want toadd at the end");
+// arr.push(end_color);
+
+// console.table("added the color at the end of array",arr);
+// arr.unshift(begining_color);
+// console.table(" added the color at the begining of the array",arr);
+
+// part b => b
+// let end_color = propmt("enter the color name you want toadd at the end");
+// arr.push(end_color)
+// console.table("added the color at the end of array",arr);
+
+// part b => c 
+// arr.unshift(yellow,orange);
+// console.table("addded the names of two more colors at the begining",arr);
+
+
+// part b => d 
+// arr.shift();
+// console.table("deleted the first color",arr);
+
+// part b => e 
+// arr.pop();
+// console.table("deleted the last color",arr);
+
+
+// part b => f 
+// let index_no_to_add = +prompt("enter index no at which you want to add a value");
+
+// let color_name = prompt("enter name of color you want to add");
+// arr.splice(index_no_to_add,0, color_name);
+// console.table("added the color: " , color_name,"at index no : ",index_number_to_add,arr);
+
+// part b => g 
+// let index_no_to_add = +prompt("enter index no at which you want to add a value");
+// let quantity = propmt("how many colors you want to delete");
+// arr.splice(index_no_to_del,quantity);
+// console.table("deleted the quantity","colors from index no:",index_no_to_del,arr);
+
+
+
+
+//Question # 10
+// let scores= [74,384,251,43,12,3544];
+
+// let sort_arr= scores.sort(function(a,b){
+
+//     if(a>b){
+//       return b  
+//     }
+//    else
+//    return -1
+// }
+// );
+// console.log("unsorted array", scores);
+// console.log("sorted array" ,sort_arr);
+
+
+//Question 11
+//# Initialize the cities array
+//cities = ["karachi", "Lahore", "islamabad", "Quetta", "peshawar"]
+
+//# Initialize the selected cities array
+//selected_cities = []
+
+//for i in range(3):
+   // selected_cities.append(cities[i])
+
+//# Print the selected cities
+//print("Selected cities:", selected_cities)
+
+
+
+
+
+//Question # 12
+// let arr = ["my","name","is","kiran","naseem"];
+
+// let joined = arr.join(" ");
+// console.log(arr.join(" "));
+
+//Question # 13
+// let arr = [];
+// arr.push("keyboard");
+// arr.push("mouse");
+// arr.push("printer");
+// arr.push("monitor");
+
+// console.log(arr);
+// console.log("0th element",arr[0]);
+// console.log("1st element",arr[1]);
+// console.log("2st element",arr[2]);
+// console.log("3rd element",arr[3]);
+
+//Question # 14
+// let arr = [];
+//  arr.push("keyboard");
+//  arr.push("mouse");
+//  arr.push("printer");
+//  arr.push("monitor");
+//  console.log(arr);
+//  arr.reverse();
+
+//  console.log("0th element",arr[0]);
+//  console.log("1st element",arr[1]);
+//  console.log("2st element",arr[2]);
+//  console.log("3rd element",arr[3]);
+
+
+
+
+
+
+
+
+
+
+
+
